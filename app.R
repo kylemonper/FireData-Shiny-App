e1d6e1ff34df22b1379b4567bf53528c499af4d7
Kyle_Monper <kyle.monper@gmail.com>
2019-02-05 20:48:35 -0800

    updated cause codes, deleted extraneous code

--- app.R
+++ app.R
@@ -22,9 +22,9 @@ library(plotly)
 #select top 100 fires 
 top100 <- fire %>% 
   select(YEAR_, FIRE_NAME,Shape_Area, CAUSE) %>% 
-  arrange(-Shape_Area)
+  arrange(-Shape_Area) 
 top100 <- top100[1:100,]
-
+top100$YEAR_ <- unfactor(top100$YEAR_)
 
 #change projection to be compatible with leaflet
 top100 <- st_transform(top100, crs = 4326)
@@ -40,9 +40,9 @@ ui <- fluidPage(
            # Date slider 
            sliderInput("date_range", 
                        label = "Select Date", 
-                       min = min(as.numeric(paste(top100$YEAR_))), 
-                       max = max(as.numeric(paste(top100$YEAR_))),
-                       value = range(as.numeric(paste(top100$YEAR_))),
+                       min = min(top100$YEAR_), 
+                       max = max(top100$YEAR_),
+                       value = range(top100$YEAR_),
                        step = 1,
                        sep = ""),
            
@@ -61,7 +61,7 @@ server <- function(input, output, session) {
   #create new reactive df based on slider date inpute in the ui
   reactive_date <- reactive({
     top100 %>%
-      filter(as.numeric(paste(YEAR_)) >= input$date_range[1] & as.numeric(paste(YEAR_)) <= input$date_range[2])
+      filter(YEAR_ >= input$date_range[1] & YEAR_ <= input$date_range[2])
   })
   
   #this outputs the map
@@ -81,7 +81,7 @@ server <- function(input, output, session) {
     leaflet(top100) %>% 
       addProviderTiles("Esri.WorldTopoMap") %>% 
       addPolygons(
-        popup = paste("<h3 style = 'color: red'> Fire Description </h3>", "<b>Fire name:</b>", top100$FIRE_NAME, "<br", "<b>Year:</b>", top100$YEAR_,"<br>", "<b>Size:</b>", top100$Shape_Area, "Sq.Meters", "<br>", "<b>Cause code</b>", top100$CAUSE, sep = " ") 
+        popup = paste("<h5 style = 'color: red'> Fire Description </h5>", "<b>Fire name:</b>", top100$FIRE_NAME, "<br", "<b>Year:</b>", top100$YEAR_,"<br>", "<b>Size:</b>", top100$Shape_Area, "Sq.Meters", "<br>", "<b>Cause code</b>", top100$CAUSE, sep = " ") 
       )
    
   })
@@ -91,7 +91,7 @@ server <- function(input, output, session) {
     leafletProxy("map", data = reactive_date()) %>%
       clearShapes() %>%
       addPolygons(
-        popup = paste("<h3 style = 'color: red'> Fire Description </h3>", "<b>Fire name:</b>", top100$FIRE_NAME, "<br", "<b>Year:</b>", top100$YEAR_,"<br>", "<b>Size:</b>", top100$Shape_Area, "Sq.Meters", "<br>", "<b>Cause code</b>", top100$CAUSE, sep = " ")
+        popup = paste("<h5 style = 'color: red'> Fire Description </h5>", "<b>Fire name:</b>", top100$FIRE_NAME, "<br", "<b>Year:</b>", top100$YEAR_,"<br>", "<b>Size:</b>", top100$Shape_Area, "Sq.Meters", "<br>", "<b>Cause code</b>", top100$CAUSE, sep = " ")
       ) 
   })
 
