8d31a3337967d32142e9ca2575accaada887eec6
Kyle_Monper <kyle.monper@gmail.com>
2019-03-05 08:58:52 -0800

    working on universial reactivity

--- simplified_debugging/app.R
+++ simplified_debugging/app.R
@@ -7,23 +7,13 @@ library(varhandle)
 library(DT)
 library(spData)
 library(shinydashboard)
-##might be a cool thing to do with our graphs:
-#app = system.file('examples', 'DT-rows', package = 'DT')
-#runApp(app)
-
-
-
-#####to do list:
-
-#make data table reactive
-#read in pdf & add data to working df
-#change layout -- make map full length on side, with datatable under the widget on the side
 
 
 #select top 100 fires 
 top100 <- fire %>% 
   select(YEAR_, FIRE_NAME,GIS_ACRES, CAUSE) %>% 
-  arrange(-GIS_ACRES)
+  arrange(-GIS_ACRES) %>% 
+  st_drop_geometry()
 
 #fix years as numeric
 top100$YEAR_ <- unfactor(top100$YEAR_)
@@ -31,24 +21,7 @@ top100$YEAR_ <- unfactor(top100$YEAR_)
 #define cause codes
 top100 <- top100[1:10,]
 
-#simplify polygons
-top100 <- top100 %>% 
-  st_simplify(dTolerance = 100)
 
-#change projection to be compatible with leaflet
-top100 <- st_transform(top100, crs = 4326)
-
-#add centriods to df to be used in marker creation
-centroid <- st_centroid(top100)
-centroid_less <- centroid %>% 
-  mutate(lat = unlist(map(centroid$geometry,1)),
-         long = unlist(map(centroid$geometry,2))) %>% 
-  select(GIS_ACRES, lat, long) %>% 
-  st_drop_geometry()
-
-# add lat long caloumns to top100 by GIS_ACRES which is only unique identifier
-top100 <- merge(top100, centroid_less, by = "GIS_ACRES") %>% 
-  arrange(YEAR_)
 
 
 
@@ -57,28 +30,21 @@ top100 <- merge(top100, centroid_less, by = "GIS_ACRES") %>%
 ##############################################################################
 
 ui <- fluidPage(
-  sidebarLayout(position = "right",
-                mainPanel(leafletOutput("map", height = 700, width = 700),
-                          tags$hr(),
-                          dataTableOutput('dto', width = 800),
-                           img(src = "rstudio.png", height = 140, width = 400)),
-                sidebarPanel( 
+  sidebarLayout(
+                mainPanel(
+                          dataTableOutput('dto', width = 800)),
+                           
+                 sidebarPanel( 
+                  uiOutput("date"),
+                  uiOutput("cause")
+                
                   
-                  # Date slider 
-                  sliderInput("date_range", 
-                              label = "Select Date", 
-                              min = min(top100$YEAR_), 
-                              max = max(top100$YEAR_),
-                              value = range(top100$YEAR_),
-                              step = 1,
-                              sep = "",
-                              width = 500),
-                  tags$img(src='bren.png')
                 )
                 
   )
   
 )
+
 
 
 ##############################################################################
@@ -87,50 +53,60 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  reactive_date <- reactive({
-    top100 %>%
-      filter(YEAR_ >= input$date_range[1] & YEAR_ <= input$date_range[2]) %>% 
-      arrange(FIRE_NAME)
+  output$date <- renderUI({
+    sliderInput("date_range", 
+                label = "Select Date", 
+                min = min(top100$YEAR_), 
+                max = max(top100$YEAR_),
+                value = range(top100$YEAR_),
+                step = 1,
+                sep = "",
+                width = 500)
   })
   
-  table <- reactive({
-    top100 %>%
-      filter(YEAR_ >= input$date_range[1] & YEAR_ <= input$date_range[2]) %>% 
-      st_drop_geometry(.)
+  output$cause <- renderUI({
+    selectInput(inputId = "cause",        
+                label = "Cause of Fire", 
+                choices = c(sort(unique(top100$CAUSE))))
   })
+  
+  
+  
+  reactive_all <- reactive({
+    
+    req(input$cause)
+    req(input$date)
+ top100 %>% 
+   filter(YEAR_ >= input$date_range[1] & YEAR_ <= input$date_range[2] & CAUSE == input$CAUSE)
+    
+
+  })
+  
+  
+  
+  ########################cause plot###########################
+  
+  
   
   # Data table
   output$dto <- renderDataTable({
-    datatable(table(), selection = "single", options = list(stateSave = TRUE))
+    datatable(reactive_all())
   })
   
-  output$map <- renderLeaflet({
-    #static background map
-    leaflet(top100) %>% 
-      addProviderTiles("Esri.WorldTopoMap") %>% 
-      addPolygons()
-  })
-
-  observe({
-   
-    leafletProxy("map", data = reactive_date()) %>%
-      clearShapes() %>%
-      addPolygons()
-        
        
-  })
+  
  
   
   # to keep track of previously selected row
-  my_icon = makeAwesomeIcon(icon = 'map-marker', markerColor = 'green', iconColor = 'white')
+ # my_icon = makeAwesomeIcon(icon = 'map-marker', markerColor = 'green', iconColor = 'white')
   
-  observeEvent(input$dto_rows_selected, {
-    row_selected <- table()[input$dto_rows_selected,] 
-    leafletProxy('map01') %>% 
-      addAwesomeMarkers(lat = row_selected$lat,
-                        lng = row_selected$long,
-                        icon = my_icon)
-})
+ # observeEvent(input$dto_rows_selected, {
+ #   row_selected <- table()[input$dto_rows_selected,] 
+  #  leafletProxy('map01') %>% 
+  #    addAwesomeMarkers(lat = row_selected$lat,
+  #                      lng = row_selected$long,
+  #                      icon = my_icon)
+#})
   
 }
 # Run the application 
